b65910dde56fb94393ee98f70705d23ff2f61e8b
Dazai Yuki <mapplesnow@gmail.com>
2020-06-15 03:10:13 +0900

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,10 @@ import React from 'react'
 import ReactDom from 'react-dom'
 
 import { Indicator, IndicateLevel } from './Indicator'
+import Tab from './Tab'
 /**部署FaceSelector的方法 */
 export function deployFaceSelector(ele:HTMLElement){
 /* ReactDom.render(<FaceSelector/>,ele) */
-ReactDom.render(<Indicator level={IndicateLevel.PRELOAD}></Indicator>,ele)
+ReactDom.render(<Tab></Tab>,ele)
 }
 deployFaceSelector(document.getElementById('c'))
