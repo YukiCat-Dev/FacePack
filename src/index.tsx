90f20730595c2123236419c52df89df19ac728bf
KotoriK <52659125+KotoriK@users.noreply.github.com>
2020-06-15 01:17:29 +0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 
-
+import React from 'react'
 import ReactDom from 'react-dom'
-import {FaceSelector} from './FaceSelector'
+
+import { Indicator, IndicateLevel } from './Indicator'
 /**部署FaceSelector的方法 */
 export function deployFaceSelector(ele:HTMLElement){
-ReactDom.render(<FaceSelector/>,ele)
+/* ReactDom.render(<FaceSelector/>,ele) */
+ReactDom.render(<Indicator level={IndicateLevel.PRELOAD}></Indicator>,ele)
 }
 deployFaceSelector(document.getElementById('c'))
