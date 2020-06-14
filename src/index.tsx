5d37c336489d698740a702e8859267c10ca0a326
KotoriK <52659125+KotoriK@users.noreply.github.com>
2020-06-15 02:16:45 +0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 import ReactDom from 'react-dom'
 
 import { Indicator, IndicateLevel } from './Indicator'
-import Tab from './Tab'
+import Tabs from './Tabs'
 /**部署FaceSelector的方法 */
 export function deployFaceSelector(ele:HTMLElement){
 /* ReactDom.render(<FaceSelector/>,ele) */
-ReactDom.render(<Tab></Tab>,ele)
+ReactDom.render(<Tabs facePackages={[{id:"114514",name:"1919",faces:[]}]}></Tabs>,ele)
 }
 deployFaceSelector(document.getElementById('c'))
