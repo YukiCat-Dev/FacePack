a9c74e97d8ea4cf0fcdc0748cd8ed5cd6c743eca
KotoriK <52659125+KotoriK@users.noreply.github.com>
2020-06-15 00:55:36 +0800

    feat:Image

---
+++ src/BaseComponentProps.ts
@@ -0,0 +1,6 @@
+import { CSSProperties } from "react";
+
+export default interface BaseComponentProps{
+    inlineStyle?:CSSProperties
+    cssClass?:string
+}
--- src/Image.tsx
+++ src/Image.tsx
@@ -1,13 +1,15 @@
-import { Component } from 'react';
-export interface ImageProps {
+import BaseComponentProps from './BaseComponentProps';
+import React from 'react';
+export interface ImageProps extends BaseComponentProps {
 
-    src:string
-    onHover:Function
-    onSelected:(e:React.MouseEvent<HTMLImageElement, MouseEvent>)=>void
+    src: string
+    onHover: Function
+    onClick: (e: React.MouseEvent<HTMLImageElement, MouseEvent>) => void
 }
- 
+
 export interface ImageState {
-    
+    nowSrc:string
+    imgUrl?: string
 }
 /**
  * 显示图片
@@ -16,12 +18,25 @@ export interface ImageState {
  * @class Image
  * @extends {Component<ImageProps, ImageState>}
  */
-class Image extends Component<ImageProps, ImageState> {
-    state = {   }
-    render() { 
-        return (<img src={this.props.src} 
-            onClick={this.props.onSelected} ></img>);
+class Image extends React.Component<ImageProps, ImageState> {
+    state: ImageState = {
+
+    }
+    render() {
+        return (<img src={this.props.src}
+            onClick={this.props.onClick} ></img>);
+    }
+    componentDidMount() {
+        let nowSrc=this.props.src
+        fetch({ url: nowSrc, method: "GET" }).then(async (response) => {
+            if (response.ok) {
+                this.setState({nowSrc, imgUrl: URL.createObjectURL(await response.blob()) })
+            }
+        })
+    }
+    componentWillUnmount(){
+        this.state.imgUrl && URL.revokeObjectURL( this.state.imgUrl)
     }
 }
- 
+
 export default Image;
