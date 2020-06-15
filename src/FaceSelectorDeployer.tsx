6bda54eff8f77c0ed62b92d215c542563f82e6b1
KotoriK <52659125+KotoriK@users.noreply.github.com>
2020-06-15 21:44:56 +0800

    调整FaceSelector的部署方式

---
+++ src/FaceSelectorDeployer.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createPopper } from '@popperjs/core';
+import { FaceSelector } from './FaceSelector'
+import { Face, FacePackage } from './base/FacePackage';
+export default class FaceSelectorDeployer {
+    private _popcorn: HTMLElement
+    private _tooltip: HTMLElement
+    private _onFaceSelected: (face: Face) => void
+    private _facePacks: Array<FacePackage>
+    constructor(popcorn: HTMLElement, tooltip: HTMLElement, facePackages: Array<FacePackage>, onFaceSelected: (face: Face) => void = () => { }) {
+        this._popcorn = popcorn
+        this._tooltip = tooltip
+        this._onFaceSelected = onFaceSelected
+        this._facePacks = facePackages
+    }
+    private _displayed: boolean=true
+    /**
+     *渲染FaceSelector
+     *
+     * @author KotoriK
+     * @memberof FaceSelectorDeployer
+     */
+    render() {
+        ReactDOM.render(<FaceSelector facePacks={this._facePacks} colCount={3} handleHide={() => {
+            this._tooltip.setAttribute("hidden", "hidden")
+        }} onFaceSelected={this._onFaceSelected} />, this._tooltip)
+        createPopper(this._popcorn, this._tooltip)
+        this._popcorn.addEventListener('click',this._handlePopcornClick.bind(this))
+        return this
+    }
+    private _handlePopcornClick(e: MouseEvent) {
+       if(this._displayed) {
+           this._tooltip.setAttribute("hidden", "hidden")
+           this._displayed=false
+        }else{
+            this._tooltip.removeAttribute("hidden")
+            this._displayed=true
+        }
+    }
+    /**
+     * 解除FaceSelector在tooltip上的挂载
+     *
+     * @author KotoriK
+     * @memberof FaceSelectorDeployer
+     */
+    unload() {
+        ReactDOM.unmountComponentAtNode(this._tooltip)
+        this._popcorn.removeEventListener('click',this._handlePopcornClick)
+        return this
+    }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,14 @@ import ReactDom from 'react-dom'
 import { Indicator, IndicateLevel } from './Indicator'
 import Tabs from './Tabs'
 /**部署FaceSelector的方法 */
-export function deployFaceSelector(ele:HTMLElement){
-/* ReactDom.render(<FaceSelector/>,ele) */
-ReactDom.render(<Tabs facePackages={[{id:"114514",name:"1919",faces:[]}]}></Tabs>,ele)
+export function deployFaceSelector(ele: HTMLElement) {
+    /* ReactDom.render(<FaceSelector/>,ele) */
+    ReactDom.render(<Tabs facePackages={[{ id: "114514", name: "1919", faces: [] }]}></Tabs>, ele)
 }
-deployFaceSelector(document.getElementById('c'))
+/* deployFaceSelector(document.getElementById('c')) */
+import facePacks from '../facepack_template/facepacks'
+import FaceSelectorDeployer from './FaceSelectorDeployer'
+const commentArea = document.getElementById('comment') as HTMLTextAreaElement
+const deployer = new FaceSelectorDeployer(document.getElementById('p'), document.getElementById('c'), facePacks, (face) => {
+    commentArea.value += `:${face.id}:`
+}).render()
