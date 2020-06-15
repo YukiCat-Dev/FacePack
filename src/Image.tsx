4a69ecea89caa99a9f3e585d7bcb56fed7650726
KotoriK <52659125+KotoriK@users.noreply.github.com>
2020-06-15 17:30:48 +0800

    Update Image.tsx

--- src/Image.tsx
+++ src/Image.tsx
@@ -1,42 +1,93 @@
 import BaseComponentProps from './BaseComponentProps';
 import React from 'react';
+import { IndicatorProps, Indicator, IndicateLevel } from './Indicator';
 export interface ImageProps extends BaseComponentProps {
-
+    id?:string
     src: string
-    onHover: Function
-    onClick: (e: React.MouseEvent<HTMLImageElement, MouseEvent>) => void
-}
+    alt?: string
+    onClick?: (e: React.MouseEvent<HTMLImageElement, MouseEvent>,id?:string) => void
+    refererPolicy?: ReferrerPolicy,
+    imgInlineStyle?:React.CSSProperties,
+    imgCSSClass?:string,
+    
 
+}
 export interface ImageState {
-    nowSrc:string
-    imgUrl?: string
+    src: { url?: string, data?: string }
+    showIndicator?: IndicatorProps
+    hovering: boolean
 }
 /**
  * 显示图片
  *
  * @author KotoriK
  * @class Image
- * @extends {Component<ImageProps, ImageState>}
+ * @extends {React.Component<ImageProps, ImageState>}
  */
-class Image extends React.Component<ImageProps, ImageState> {
-    state: ImageState = {
-
+export default class Image extends React.Component<ImageProps, ImageState> {
+    constructor(props: ImageProps) {
+        super(props)
+        this.state = {
+            src: {url:props.src},
+            showIndicator: { level: IndicateLevel.PRELOAD },
+            hovering: false
+        }
     }
-    render() {
-        return (<img src={this.props.src}
-            onClick={this.props.onClick} ></img>);
+
+    /**
+     * 懒加载图片
+     * @author KotoriK
+     * @memberof Image
+     */
+    loadImg() {
+        let nowSrc = this.props.src
+        fetch(nowSrc, { method: "GET", referrerPolicy: this.props.refererPolicy || "no-referrer" })
+            .then(async (response) => {
+                if (response.ok) {
+                    this.setState({ src: { url: nowSrc, data: URL.createObjectURL(await response.blob()) }, showIndicator: undefined })
+                } else {
+                    switch (response.status) {
+                        case 404:
+                            this.setState({ showIndicator: { level: IndicateLevel.ERROR, description: "表情包不见了！" } })
+                            break
+                        case 500://服务器不可用
+                            this.setState({ showIndicator: { level: IndicateLevel.ERROR, description: "服务器暂时不可用，请稍后再试。" } })
+                            break
+                        default:
+                            this.setState({ showIndicator: { level: IndicateLevel.ERROR, description: `HTTP ${response.status}:${response.statusText}` } })
+                    }
+                }
+            },(reason)=>{
+                this.setState({showIndicator: { level: IndicateLevel.ERROR,description:reason}})
+            })
+            
     }
     componentDidMount() {
-        let nowSrc=this.props.src
-        fetch({ url: nowSrc, method: "GET" }).then(async (response) => {
-            if (response.ok) {
-                this.setState({nowSrc, imgUrl: URL.createObjectURL(await response.blob()) })
-            }
-        })
+        this.loadImg()
     }
-    componentWillUnmount(){
-        this.state.imgUrl && URL.revokeObjectURL( this.state.imgUrl)
+    componentDidUpdate() {
+        if (this.props.src !== this.state.src.url) {
+            this.loadImg()
+        }
+    }
+    componentWillUnmount() {
+        //释放资源
+        if (this.state.src.data) URL.revokeObjectURL(this.state.src.data)
+    }
+    prerender() {
+        if (this.state.src.data) {
+            return (
+                <img src={this.state.src.data}
+                    onClick={(e)=>this.props.onClick(e,this.props.id)}
+                    onPointerEnter={() => this.setState({ hovering: true })}
+                    onPointerOut={() => this.setState({ hovering: false })} alt={this.props.alt} style={{ ...this.props.imgInlineStyle }} className={this.props.imgCSSClass}></img>
+            );
+        }
+        if (this.state.showIndicator) {
+            return (<Indicator {...this.state.showIndicator} />)
+        }
+    }
+    render() {
+        return (<div className={this.props.cssClass} style={this.props.inlineStyle}>{this.prerender()}</div>)
     }
 }
-
-export default Image;
