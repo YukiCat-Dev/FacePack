8f08c406f43fd05308ab856fd9ab8cac523e03cc
KotoriK <52659125+KotoriK@users.noreply.github.com>
2020-06-21 19:09:13 +0800

    Update TableView.tsx

--- src/FaceSelector/component/TableView.tsx
+++ src/FaceSelector/component/TableView.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { FacePackage } from '../../FacePackage';
 import FaceView from './FaceView'
 import BaseComponentProps from './BaseComponentProps';
-export interface TableViewProps extends BaseComponentProps{
+export interface TableViewProps extends BaseComponentProps {
 
     /**
      * 设定一行显示多少列
@@ -19,7 +19,7 @@ export interface TableViewProps extends BaseComponentProps{
      * @memberof TableViewProps
      */
     facePackage: FacePackage
-    onImageSelected: (face_pos:number) => void
+    onImageSelected: (face_pos: number) => void
 }
 
 export interface TableViewState {
@@ -32,9 +32,9 @@ export default class TableView extends React.Component<TableViewProps, TableView
         this.props.onImageSelected(pos)
     }
     render() {
-        const facePackName=this.props.facePackage.name
+        const facePackId = this.props.facePackage.id
         const faces = this.props.facePackage.faces.map((value, index) => {
-            return (<td key={facePackName+'_'+index} style={{ textAlign: "center", border: "1pt solid" }}><FaceView src={value.url} face_pos={index} style={{ width: "30px", height: "30px" }} onClick={this.handleImageClick.bind(this)} global={this.props.global} /></td>)
+            return (<td key={facePackId + '_' + index} style={{ textAlign: "center", border: "1pt solid" }}><FaceView src={value.url} face_pos={index} style={{ width: "30px", height: "30px" }} onClick={this.handleImageClick.bind(this)} global={this.props.global} /></td>)
         })
         const colCount = this.props.colCount
         const rowCount = Math.ceil((faces.length / colCount))
