2e5d4818d37a09c5886a0083c850bdfaa2dd9620
KotoriK <52659125+KotoriK@users.noreply.github.com>
2020-06-21 19:47:13 +0800

    Update FaceDisplay.ts
    使用util/template

--- src/FaceDisplay/FaceDisplay.ts
+++ src/FaceDisplay/FaceDisplay.ts
@@ -1,4 +1,5 @@
-import { FacePackage } from "../FacePackage"
+import { FacePackage, getFaceFullUrl } from "../FacePackage"
+import { processTemplate } from "../util/template"
 
 export default class FaceDisplay {
     private _className: string
@@ -13,58 +14,15 @@ export default class FaceDisplay {
         this.RIGHT_BRACKET = rightBracket
         for (const pack of facePackages) {
             for (const face of pack.faces) {
-                this._faceMap.set(face.id, face.url)
+                this._faceMap.set(face.id,face.url)
             }
         }
     }
     render(onElement: HTMLElement|Element) {
-        onElement.innerHTML = this.processInnerHTML(onElement.innerHTML)
-    }
-    processInnerHTML(innerHTML: string) {
-        let inBracket = false
-        let newInnerHTML = ""
-        let bracketContent = ""
-        for (const char of innerHTML) {
-            switch (char) {
-                case this.LEFT_BRACKET:
-                    if (inBracket) {
-                        if (this.LEFT_BRACKET == this.RIGHT_BRACKET) {
-                            inBracket = false
-                            newInnerHTML += this.replacePlaceHolder(bracketContent)
-                            bracketContent = ""
-                        } else {
-                            inBracket = false
-                            newInnerHTML += `${this.LEFT_BRACKET}bracketContent${this.LEFT_BRACKET}`
-                            bracketContent = ""
-                        }
-                    } else {
-                        inBracket = true
-                    }
-                    break
-                case this.RIGHT_BRACKET:
-                    if (inBracket) {
-                        inBracket = false
-                        newInnerHTML += this.replacePlaceHolder(bracketContent)
-                        bracketContent = ""
-                    } else {
-                        newInnerHTML += char
-                    }
-                    break
-                default:
-                    if (inBracket) {
-                        bracketContent += char
-                    } else {
-                        newInnerHTML += char
-                    }
-            }
-        }
-        if (bracketContent != '') {
-            newInnerHTML += bracketContent
-        }
-        return newInnerHTML
+        onElement.innerHTML = processTemplate(this.LEFT_BRACKET,this.RIGHT_BRACKET,this.replacePlaceHolder.bind(this),onElement.innerHTML)
     }
     replacePlaceHolder(placeHolder: string) {
-        let url = this._faceMap.get(placeHolder)
+        const url = this._faceMap.get(placeHolder)
         if (url) {
             return `<img class="${this._className}" src="${url}" style="${this._inlineStyle} max-height:5vh;" alt="${this.LEFT_BRACKET}${placeHolder}${this.RIGHT_BRACKET}"/>`
         } else {
