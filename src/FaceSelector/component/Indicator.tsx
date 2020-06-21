137058bf9e29b647a4de418f89cb1d3cf90b123a
KotoriK <52659125+KotoriK@users.noreply.github.com>
2020-06-21 17:10:23 +0800

    Update Indicator.tsx
    去除没有用上的svg

--- src/FaceSelector/component/Indicator.tsx
+++ src/FaceSelector/component/Indicator.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import BaseComponentProps from './BaseComponentProps';
 const SVG_PRELOAD = "https://cdn.jsdelivr.net/gh/YukiCat-Dev/FacePack/static/Ripple-1.3s-237px.svg"
-const SVG_WARNING = "https://cdn.jsdelivr.net/gh/YukiCat-Dev/FacePack/static/warning.svg"
-const SVG_INFO = "https://cdn.jsdelivr.net/gh/YukiCat-Dev/FacePack/static/info.svg"
+/* const SVG_WARNING = "https://cdn.jsdelivr.net/gh/YukiCat-Dev/FacePack/static/warning.svg"
+const SVG_INFO = "https://cdn.jsdelivr.net/gh/YukiCat-Dev/FacePack/static/info.svg" */
 const SVG_ERROR = "https://cdn.jsdelivr.net/gh/YukiCat-Dev/FacePack/static/error.svg"
 export enum IndicateLevel {
     PRELOAD , 
-    INFO , 
-    WARNING , 
+/*     INFO , 
+    WARNING ,  */
     ERROR 
 }
-const IndicateLevelSVG=[SVG_PRELOAD,SVG_WARNING,SVG_INFO,SVG_ERROR]
+const IndicateLevelSVG=[SVG_PRELOAD,/* SVG_WARNING,SVG_INFO, */SVG_ERROR]
 export interface IndicatorProps extends BaseComponentProps {
     level: IndicateLevel
     description?: string
