e661a19a8eaa40dc0bfd7ecc0e451e1a039ee355
KotoriK <52659125+KotoriK@users.noreply.github.com>
2020-06-27 22:20:56 +0800

    Update deploy-display.ts
    现在会对整篇文章的表情代码进行替换；
    删除了只使用了一次的常量

--- src/deploy-display.ts
+++ src/deploy-display.ts
@@ -1,12 +1,9 @@
 /**部署样板 */
 import { importExternalFacePacks } from './FacePacksImporter'
+import FaceDisplay from './FaceDisplay/FaceDisplay'
 (async () => {
-    const FaceDisplay = (await import('./FaceDisplay/FaceDisplay')).default
-    const facePacks = await importExternalFacePacks('https://cdn.jsdelivr.net/gh/YukiCat-Dev/yukicat.facepack/facepacks.json')
-    /*     const facePacks = DefaultFacePack
-     */
-    let display=new FaceDisplay(facePacks)
-    document.querySelectorAll('.ct-comment-content.entry-content').forEach((v)=>{
+    let display=new FaceDisplay(await importExternalFacePacks('https://cdn.jsdelivr.net/gh/YukiCat-Dev/yukicat.facepack/facepacks.json'))
+    document.querySelectorAll('article.hentry').forEach((v)=>{
         display.render(v)
     })
 })()
