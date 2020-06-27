f98670541a09242e3b71589bcbfdd4459f8ac77e
KotoriK <52659125+KotoriK@users.noreply.github.com>
2020-06-27 22:20:17 +0800

    Update deploy-selector.ts
    更改引用方式；删除只使用了一次的常量

--- src/deploy-selector.ts
+++ src/deploy-selector.ts
@@ -1,12 +1,14 @@
 /**部署样板 */
 import { importExternalFacePacks } from './FacePacksImporter'
+import FaceSelectorDeployer from './FaceSelector/FaceSelectorDeployer'
 (async () => {
-    const FaceSelectorDeployer = (await import('./FaceSelector/FaceSelectorDeployer')).default
     const commentArea = document.getElementById('comment') as HTMLTextAreaElement
-    const facePacks = await importExternalFacePacks('https://cdn.jsdelivr.net/gh/YukiCat-Dev/yukicat.facepack/facepacks.json')
 /*     const facePacks = DefaultFacePack
  */    new FaceSelectorDeployer({
-        popcorn: document.getElementById('show-fs'), tooltip: document.getElementById('fs-c'), facePackages: facePacks, onFaceSelected: 
+        popcorn: document.getElementById('show-fs'), 
+        tooltip: document.getElementById('fs-c'), 
+        facePackages: await importExternalFacePacks('https://cdn.jsdelivr.net/gh/YukiCat-Dev/yukicat.facepack/facepacks.json'), 
+        onFaceSelected: 
         (pack,face) => {
             commentArea.value += `:${pack.id}.${face.id}:`
         }, popperOptions: { placement: 'top' }
