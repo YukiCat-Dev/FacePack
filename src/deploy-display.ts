ba75536c01fd10897de78193b026ccf83fdc0cdd
KotoriK <52659125+KotoriK@users.noreply.github.com>
2020-06-28 20:31:29 +0800

    Update deploy-display.ts
    更改选择器

--- src/deploy-display.ts
+++ src/deploy-display.ts
@@ -3,7 +3,7 @@ import { importExternalFacePacks } from './FacePacksImporter'
 import FaceDisplay from './FaceDisplay/FaceDisplay'
 (async () => {
     const display = new FaceDisplay(await importExternalFacePacks('https://cdn.jsdelivr.net/gh/YukiCat-Dev/yukicat.facepack/facepacks.json'))
-    document.querySelectorAll('article.hentry p').forEach((v) => {
+    document.querySelectorAll('article.hentry p:not(.ct-respond-form-textarea):not(.form-submit)').forEach((v) => {
         display.render(v)
     })
 })()
