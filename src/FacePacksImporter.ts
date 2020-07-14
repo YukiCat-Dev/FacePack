1490470e3bbb84b1b507f1c06554e6350881427a
KotoriK <52659125+KotoriK@users.noreply.github.com>
2020-07-14 22:32:39 +0800

    Update FacePacksImporter.ts

--- src/FacePacksImporter.ts
+++ src/FacePacksImporter.ts
@@ -3,11 +3,11 @@ import { FacePackage, getFaceFullUrl } from './FacePackage'
 export async function importExternalFacePacks(url: string) {
     const json: Array<FacePackage> = await importJSON(url)
     if (json) {
-        json.forEach((pack) => {
+        for(const pack of json){
             pack.faces.forEach((face) => {
                 face.url = getFaceFullUrl(face, pack)
             })
-        })
+        }
         return json
     }
     throw new Error(`Try to load FacePacks from '${url}' failed.`)
