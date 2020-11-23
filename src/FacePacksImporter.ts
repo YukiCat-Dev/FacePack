eae0a0e10519deba4bbbc4a7bf0f32d3d089fbc5
KotoriK <52659125+KotoriK@users.noreply.github.com>
2020-11-23 21:49:15 +0800

    Update FacePacksImporter.ts
    修正类型

--- src/FacePacksImporter.ts
+++ src/FacePacksImporter.ts
@@ -1,15 +1,15 @@
 import { importJSON } from './util/importJSON'
-import {  FacePackageDefine, getFaceFullUrl } from './FacePackage'
+import { FacePackage, FacePackageDefine, getFaceFullUrl } from './FacePackage'
 export async function importExternalFacePacks(url: string) {
     const json: Array<FacePackageDefine> = await importJSON(url)
     if (json) {
         preprocessFacePack(json)
-        return json
+        return json as Array<FacePackage>
     }
     throw new Error(`Try to load FacePacks from '${url}' failed.`)
 }
 export function preprocessFacePack(facepacks: Array<FacePackageDefine>) {
     for (const pack of facepacks) {
-        pack.faces=pack.faces.map((face) => getFaceFullUrl(face, pack))
+        pack.faces = pack.faces.map((face) => getFaceFullUrl(face, pack))
     }
 }
